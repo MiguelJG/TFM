0096a5da35307c03e37422285b771a4c927598b8
Miguel <jimenezgomismiguel@gmail.com>
2021-06-24 13:22:48 +0200

    Estilo añadido
    Se ha añadido el estilo en css y se han corregido pequeños errores de ejecución.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# TMF
-TFM del Máster en Inteligencia Artificial de UNIR
+# Propuesta y desarrollo deun modelo de Gobierno y Control de IAs
+
+
+Trabajo Fin de Máster presentado por:Miguel Jiménez Gomis
+Dirigido por:Esteve Nadal Roig
+
+## Contenidos de este Github
+
+* Código de la demo técnica de gestión del modelo de Gobierno y Control de IAs propuesta.
+* Código de generación de KPIs de QA.
--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ function login() {
     xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         if (this.responseText == "208") {
-          document.getElementById("main").innerHTML = "<div class='error'>Error 208, usuario o clave inválido</div>";
+          document.getElementById("main").innerHTML = "<div class='error'>Error 208, usuario o clave inválido </div>" + '<img alt="" src="./img/innvaliduser.png" width=100 height=100>';
         } else{
           let response = this.responseText.split(";");
           let name = "<div class='userData'>" + response[0] + "</div>";
@@ -75,7 +75,7 @@ function moreinfo(id) {
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       if (this.responseText == "208") {
-        document.getElementById("main").innerHTML = "<div class='error'>Error 208, usuario o clave inválido</div>";
+        document.getElementById("main").innerHTML = "<div class='error'>Error 208, usuario o clave inválido </div>" + '<img alt="" src="./img/innvaliduser.png" width=100 height=100>';
       } else{
         let response = String(this.responseText).split("|");
         let nombre = response[0].split(";")[0]; //Se seleccionan el nombre del usuario
@@ -83,7 +83,7 @@ function moreinfo(id) {
         response = response[1].split(";"); // Se separa el listado de KPIs
         let name = "<div class='userData'>" + nombre + "</div>";
         let table = "<table class='IATable'><tr><th>Tipo de aplicación</th><th>MAE</th><th> RMSE</th><th>RMLSE</th><th>Prec</th><th>Recall</th><th>Acurracy</th><th>Sensitivity</th><th>Specificity</th><th>F_measure</th></tr>";       
-        let back = '<button onclick="backtologin()">Click me</button>';
+        let back = '<button class="logoff" onclick="backtologin(↩️)"></button>';
         let confusion = ""; //Direccion a la matriz de confusion
         for(let i = 0; i < response.length; i++){
           table += "<tr>";
@@ -94,11 +94,11 @@ function moreinfo(id) {
             table += "<td>";
             if(j == 0){
               if(elements[j] == "1"){//Clasificación
-                table += '<img alt="Clasificación" src="/img/clasif.png" width=10" height="10">';
+                table += '<img alt="Clasificación" src="./img/clasif.png" width=10" height="10">';
               } else if(elements[j] == "2"){//Regresión
-                table += '<img alt="Regresión" src="/img/regres.png" width=10" height="10">';
+                table += '<img alt="Regresión" src="./img/regres.png" width=10" height="10">';
               }else{ //Other
-                table += '<img alt="Otros" src="/img/otrot.png" width=10" height="10">';
+                table += '<img alt="Otros" src="./img/otrot.png" width=10" height="10">';
               }
             }else{
               table += elements[j];
@@ -108,19 +108,20 @@ function moreinfo(id) {
           table += "</tr>"
         }
         table += "</table>"
-        let documentacion = "";
+        let documentacion = "<div class='DocIA'>";
+        let tipriesgo = "";
         for(let i = 0; i < info.length; i++){
           if(i == 5){
             if(info[i] == "1"){
-              documentacion += '<img alt="Riesgo intolerable" src="/img/riesg1.png" width=10" height="10">';
+              tipriesgo += '<img alt="Riesgo intolerable" src="./img/riesg1.png" width=10" height="10">';
             }else if(info[i] == "2"){
-              documentacion += '<img alt="Riesgo Elevado" src="/img/riesg2.png" width=10" height="10">';
+              tipriesgo += '<img alt="Riesgo Elevado" src="./img/riesg2.png" width=10" height="10">';
             } else if(info[i] == "3"){
-              documentacion += '<img alt="Riesgo Medio" src="/img/riesg3.png" width=10" height="10">';
+              tipriesgo += '<img alt="Riesgo Medio" src="./img/riesg3.png" width=10" height="10">';
             } else if(info[i] == "4"){
-              documentacion += '<img alt="Riesgo mínimo" src="/img/riesg4.png" width=10" height="10">';
+              tipriesgo += '<img alt="Riesgo mínimo" src="./img/riesg4.png" width=10" height="10">';
             }else{
-              documentacion += '<img alt="otros Riesgos" src="/img/riesgother.png" width=10" height="10">';
+              tipriesgo += '<img alt="otros Riesgos" src="./img/riesgother.png" width=10" height="10">';
             }
           }else if(i == 0){
             documentacion += "<div class=" + i +"> ID: " + info[i] +"</div>"; // ID
@@ -131,22 +132,23 @@ function moreinfo(id) {
           }else if(i == 3){
             documentacion += "<div class=" + i +">Fecha de creación: " + info[i] +"</div>"; //Fecha de Creación
           }else if(i == 4){
-            documentacion += "<div class=" + i +">Documentación general" + '<a href="' + info[i] + '">' + '<img alt="Documentacion General" src="/img/doc.png" width=10" height="10"></a>' +"</div>"; // Documentacion
+            documentacion += "<div class=" + i +">Documentación general" + '<a href="' + info[i] + '">' + '<img alt="Documentacion General" src="./img/doc.png" width=10" height="10"></a>' +"</div>"; // Documentacion
           }else if(i == 6){
-            documentacion += "<div class=" + i +">Documentación de la  Tipología" + '<a href="' + info[i] + '">' + '<img alt="Documentacion Tipología" src="/img/doc.png" width=10" height="10"></a>' +"</div>"; //Doc tipología
+            documentacion += "<div class=" + i +">Documentación de la  Tipología" + '<a href="' + info[i] + '">' + '<img alt="Documentacion Tipología" src="./img/doc.png" width=10" height="10"></a>' +"</div>"; //Doc tipología
           }else if(i == 7){
-            documentacion += "<div class=" + i +">Documentación QA" + '<a href="' + info[i] + '">' + '<img alt="Documentacion QA" src="/img/doc.png" width=10" height="10"></a>' +"</div>";//QA
+            documentacion += "<div class=" + i +">Documentación QA" + '<a href="' + info[i] + '">' + '<img alt="Documentacion QA" src="./img/doc.png" width=10" height="10"></a>' +"</div>";//QA
           }else if(i == 8){
-            documentacion += "<div class=" + i +">Documentación Sesgos" + '<a href="' + info[i] + '">' + '<img alt="Documentacion Sesgos" src="/img/doc.png" width=10" height="10"></a>' +"</div>"; //Sesgos
+            documentacion += "<div class=" + i +">Documentación Sesgos" + '<a href="' + info[i] + '">' + '<img alt="Documentacion Sesgos" src="./img/doc.png" width=10" height="10"></a>' +"</div>"; //Sesgos
           }else if(i == 9){
-            documentacion += "<div class=" + i +">Documentación Comité ético" + '<a href="' + info[i] + '">' + '<img alt="Documentacion Comité ético" src="/img/doc.png" width=10" height="10"></a>' +"</div>"; //Sesgos
+            documentacion += "<div class=" + i +">Documentación Comité ético" + '<a href="' + info[i] + '">' + '<img alt="Documentacion Comité ético" src="./img/doc.png" width=10" height="10"></a>' +"</div>"; //Sesgos
           }
           else{
             documentacion += "<div class=" + i +">" + info[i] +"</div>";
           }
             
         }
-        document.getElementById("main").innerHTML = name + documentacion + table + confusion + back;
+        documentacion += "</div>";
+        document.getElementById("main").innerHTML = name +documentacion + table + tipriesgo + confusion + back;
       }
 
     }
